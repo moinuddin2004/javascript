45b493ac0280ae4ea060ca5ae8ea663c2958bc17
syed moin uddin <121613955+moinuddin2004@users.noreply.github.com>
2023-02-20 14:41:41 +0500

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,115 @@
+// ONE 
+// for (let i = 0; i < 5; i++) {
+//     document.write('hello world <br>')
+    
+// }
+// TWO
+// for (let i = 1; i <=10; i++) {
+    
+//     document.write(i +'<br>')
+// }
+// THREE
+// var num1=+prompt('enter table')
+// var ran=+prompt('enter range')
+// for (let i = 1; i <=ran; i++) {
+
+//   document.write(num1+' * '+i+' ==> '+num1*i+'<br>')
+    
+// }
+// FOUR
+// var a=['nokia','apple','samsung','realme']
+// for (let i = 0; i < a.length; i++) {
+//     document.write( a[i]+'<br>')
+    
+// }
+// FIVE
+
+// var n = parseInt(prompt("Enter the number of items"));
+// var arr = [];
+
+// for (let i = 0; i < n; i++) {
+//   let item = prompt("Enter item " + (i+1));
+//   arr.push(item);
+// }
+// document.write(arr);
+// SIX
+
+// var a=prompt("enter your food")
+// var upper=a.slice(0,1).toUpperCase()
+// var lower=a.slice(1).toLowerCase()
+// var food=upper+lower
+// var arr=["Cookie","Cake","Chips","Apple"]
+// var flag=true
+// for (var i = 0; i < arr.length; i++) {
+//     if(food==arr[i]){
+       
+//      document.write(arr[i]+' is available in '+ i + 'index '+ 'of our bakery')
+//       flag = false
+//     }
+    
+// }
+// if(flag==true)
+// {
+//     document.write('not available')
+// }
+// SEVEN
+// var A = [ [1,2,3] , [4,5,6] , [7,8,9] ]
+// for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//         document.write(A[i][j])
+        
+//     }
+//     document.write('<br>')
+// }
+// EIGHT
+// var num =+prompt('enter')
+// for (let i = num ; i >= 0; i-0.5) {
+//    console.log(i)
+//    // document.write(i)
+    
+// }
+// NINE
+// for (let i = 1; i <=20; i++) {
+//     if(i%2==0){
+//       document.write(i+'even'+'<br>')
+//     }
+//    else{
+//       document.write(i+'odd'+'<br>')
+//    }
+// }
+// TEN
+// var odd=1
+// for (let i = 1; i <=7 ; i++) {
+//    if(i%2==0){
+//       continue
+//     }
+//    else{
+//       odd*=i;
+  
+//    }
+// }
+// document.write(odd)
+// ELEVEN
+// for (let i = 0; i < 4; i++) {
+//   for (let j = 0; j < 5; j++) {
+//    document.write('*')
+   
+//   }
+//    document.write('<br>')
+// }
+// TWELVE
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < i; j++) {
+//    document.write('*')
+   
+//   }
+//    document.write('<br>')
+// }
+// THIRTEEN
+// for (let i = 5; i > 0; i--) {
+//   for (let j = i; j > 0; j--) {
+//    document.write('*')
+   
+//   }
+//    document.write('<br>')
+// }
---
+++ 8.js
@@ -0,0 +1,49 @@
+// var fname=prompt("enter your first name")
+// var lname=prompt("enter your last name")
+// var fullname=fname +lname
+// document.write(fullname +" welcome")
+// TWO
+// let str =prompt("enter")
+// let strLength = str.length
+// console.log(strLength)
+// THREE
+// let str = "pakistani";
+// let index = str.indexOf('n');
+// console.log(index); 
+// FOUR
+// let str = "Hello,World!";
+// let index = str.lastIndexOf("l");
+// console.log(index);
+// FIVE
+// var input=prompt("enter")
+// var upper=input.slice(0).toUpperCase()
+// document.write(upper)
+// SIX
+// var input=prompt("enter")
+// var upper=input.slice(0,1).toUpperCase()
+// document.write(upper)
+// SEVEN
+// var floatNum = 35.36;
+// var  intNum = Math.floor(floatNum * 100); 
+// console.log(intNum);
+// EIGHT
+// var a= 476
+// document.write(typeof a)
+// var str =a.toString()
+// document.write(typeof str)
+
+
+
+// 26 above
+
+// ONE
+// document.write(Math.floor(45.5)+'<br>')
+// document.write(Math.ceil(44.4)+'<br>')
+// document.write(Math.round(44.6)+'<br>')
+
+// TWO
+// console.log(Math.floor(-32.4)+'<br>')
+// console.log(Math.ceil(-32.4)+'<br>')
+// console.log(Math.round(-31.8)+'<br>')
+// THREE
+
